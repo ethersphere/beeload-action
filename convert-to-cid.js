bb602ff9fe0b1407fd7351432ed14bed24e860a9
Attila Gazso <agazso@gmail.com>
2022-01-20 14:28:43 +0100

    feat: use swarm-cid-js

--- convert-to-cid.js
+++ convert-to-cid.js
@@ -1,20 +1,8 @@
 #!/usr/bin/env node
 
-const CID = require('cids')
-
-function hexToBytes(hex) {
-  const bytes = new Uint8Array(hex.length / 2)
-  for (let i = 0; i < bytes.length; i++) {
-    const hexByte = hex.substr(i * 2, 2)
-    bytes[i] = parseInt(hexByte, 16)
-  }
-
-  return bytes
-}
+const swarmCID = require('@ethersphere/swarm-cid')
 
 const input = process.argv[2]
-const hashBytes = hexToBytes(input)
-const multihash = new Uint8Array([0x1b, hashBytes.length, ...hashBytes])
-const cid = new CID(1, 'dag-pb', multihash)
+const cid = swarmCID.encodeManifestReference(input)
 
 process.stdout.write(cid.toString())
